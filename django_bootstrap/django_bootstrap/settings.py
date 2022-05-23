cd7d36f2b2f0a0017479cbc1022e94356c51f82f
Akintola97 <akinoyetayo97@gmail.com>
2022-05-22 19:19:43 -0500

    update

--- django_bootstrap/django_bootstrap/settings.py
+++ django_bootstrap/django_bootstrap/settings.py
@@ -137,13 +137,6 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-#email 
-#EMAIL_BACKEND = 'django_core.mail.backends.smtp.EmailBackend'
-#EMAIL_HOST = 'smtp.mail.yahoo.com'
-#EMAIL_PORT = 587
-#EMAIL_USE_TLS = False #this is a security thing #check to see if gmail needs this part 
-#EMAIL_HOST_USER = 'akinoyetayo@rocketmail.com'
-#EMAIL_HOST_PASSWORD = '' #this is perhaps your actual password
 
 
 
